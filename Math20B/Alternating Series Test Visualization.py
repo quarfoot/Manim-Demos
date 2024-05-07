f534665f69a9c84224479a201e792210bb29cd46
David Quarfoot <110117821+quarfoot@users.noreply.github.com>
2024-05-07 12:57:34 -0700

    Alternating Series Test Visualization.py

--- Math20B/Alternating Series Test Visualization.py
+++ Math20B/Alternating Series Test Visualization.py
@@ -10,24 +10,28 @@ class ASTDemo(Scene):
         def b(n):
             return 1 / n
 
+        self.camera.background_color = WHITE
         # global variables
         axis = NumberLine(x_range=[-0.1, 1.1, 2], length=10, numbers_to_include=[0]).shift(DOWN)
-        intro = MathTex(r"\text{How does the Alternating Series Test work?", font_size=48)
+        axis.set_color(BLACK)
+        intro = MathTex(r"\text{How does the Alternating Series Test work?", font_size=48, color=BLACK)
         series = MathTex(r"s =", r"b_1", r"-b_2", r"+b_3", r"-b_4", r"+b_5", r"-b_6", r"+\cdots",
-                         r" = \displaystyle\sum_{n=1}^\infty (-1)^{n+1} b_n", font_size=36).to_edge(UP, MED_SMALL_BUFF)
+                         r" = \displaystyle\sum_{n=1}^\infty (-1)^{n+1} b_n",
+                         color=BLACK, font_size=36).to_edge(UP, MED_SMALL_BUFF)
 
         s = ValueTracker(0)  # keeps track of the partial sums so far
         sumLine = always_redraw(lambda: DashedLine(axis.n2p(s.get_value()) + 2 * DOWN,
                                                    axis.n2p(s.get_value()) + 3 * UP,
-                                                   color=GREEN))
-        sumText = always_redraw(lambda: MathTex(r"\text{sum so far}", font_size=20).next_to(sumLine, DOWN, SMALL_BUFF))
-        skip = False
+                                                   color=PURE_RED))
+        sumText = always_redraw(lambda: MathTex(r"\text{sum so far}",
+                                                color=PURE_RED, font_size=20).next_to(sumLine, DOWN, SMALL_BUFF))
+        skip = True
 
         ############### ANIMATIONS ################
 
         #############
         ### Section 1 - Initial setup
-        self.next_section(skip_animations=skip)
+        self.next_section(skip_animations=not skip)
         self.play(FadeIn(intro, shift=UP, run_time=2))
         self.wait(2)
         self.play(FadeOut(intro))
@@ -36,13 +40,13 @@ class ASTDemo(Scene):
 
         #############
         ### Section 2 - Visualizing sum of first 6 terms
-        self.next_section(skip_animations=skip)
+        self.next_section(skip_animations=not skip)
         self.play(FadeIn(sumLine, sumText, run_time=2))
         self.wait(2)
         sum = 0  # an accumulator variable for the partial sums
         # consider partial sums for first 6 terms
-        termHighlight = SurroundingRectangle(series[1])  # a bound around terms added so far
-        rectangleLabel = MathTex(r"s_1", font_size=24, color=YELLOW).next_to(termHighlight, UP, SMALL_BUFF)
+        termHighlight = SurroundingRectangle(series[1], color=PURE_RED)  # a bound around terms added so far
+        rectangleLabel = MathTex(r"s_1", font_size=24, color=PURE_RED).next_to(termHighlight, UP, SMALL_BUFF)
         arrowStart = axis.n2p(0)
         for n in range(1, 7):
             # term updating
@@ -51,19 +55,19 @@ class ASTDemo(Scene):
 
             # axis updating
             newtick = axis.get_tick(sum)
-            newlabel = MathTex(r"s_{" + str(n) + "}", font_size=24).next_to(newtick, DOWN, SMALL_BUFF)
+            newlabel = MathTex(r"s_{" + str(n) + "}", color=PURE_BLUE, font_size=24).next_to(newtick, DOWN, SMALL_BUFF)
 
             # arrow updating
             arrowEnd = axis.n2p(sum)
-            newarrow = CurvedArrow(arrowStart, arrowEnd, color=RED)
-            arrowlabel = MathTex(series.tex_strings[n], color=RED,
+            newarrow = CurvedArrow(arrowStart, arrowEnd, color=PURE_BLUE)
+            arrowlabel = MathTex(series.tex_strings[n], color=PURE_BLUE,
                                  font_size=24).next_to(newarrow, DOWN if n % 2 == 1 else UP, SMALL_BUFF)
             arrowGroup = VGroup(newarrow, arrowlabel)
             arrowStart = arrowEnd
 
             # new term highlighting
-            newRectangle = SurroundingRectangle(series[1:(n + 1)])
-            newRectangleLabel = newlabel.copy().next_to(newRectangle, UP, SMALL_BUFF).set_color(YELLOW)
+            newRectangle = SurroundingRectangle(series[1:(n + 1)], color=PURE_RED)
+            newRectangleLabel = newlabel.copy().next_to(newRectangle, UP, SMALL_BUFF).set_color(PURE_RED)
 
             # animations
             if n == 1:
@@ -81,9 +85,9 @@ class ASTDemo(Scene):
 
         #############
         ### Section 3 - Animating sum of terms beyond first six
-        self.next_section(skip_animations=skip)
-        alltermsRect = SurroundingRectangle(series[1:8])
-        alltermsLabel = MathTex(r"s", color=YELLOW, font_size=24).next_to(alltermsRect, UP, SMALL_BUFF)
+        self.next_section(skip_animations=not skip)
+        alltermsRect = SurroundingRectangle(series[1:8], color=PURE_RED)
+        alltermsLabel = MathTex(r"s", color=PURE_RED, font_size=24).next_to(alltermsRect, UP, SMALL_BUFF)
         self.play(Transform(termHighlight, alltermsRect),
                   Transform(rectangleLabel, alltermsLabel),
                   run_time=2)
@@ -100,8 +104,8 @@ class ASTDemo(Scene):
             timesIndex += 1
         self.wait(2)
 
-        sTick = axis.get_tick(sum).set_color(YELLOW)
-        sLabel = MathTex(r"s", color=YELLOW, font_size=24).next_to(sTick, DOWN, SMALL_BUFF)
+        sTick = axis.get_tick(sum).set_color(PURE_RED)
+        sLabel = MathTex(r"s", color=PURE_RED, font_size=24).next_to(sTick, DOWN, SMALL_BUFF)
         self.play(FadeIn(sTick, sLabel))
         self.wait(1)
         self.play(FadeOut(sumLine, sumText))
