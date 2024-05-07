89a9d5f22219ba4313fc9543f19551cc58b1ec26
David Quarfoot <110117821+quarfoot@users.noreply.github.com>
2024-05-07 07:33:18 -0700

    Integral Test for Series Visual Intuition.py

--- Math20B/Integral Test for Series Visual Intuition.py
+++ Math20B/Integral Test for Series Visual Intuition.py
@@ -8,19 +8,20 @@ class ITViz(Scene):
         def f(x):
             return 3 / x
 
+        self.camera.background_color = WHITE
         question = MathTex(r"\text{Visual Intuition Behind}\\",
-                           r"\text{The Integral Test for Series}").arrange(DOWN)
+                           r"\text{The Integral Test for Series}", color=BLACK).arrange(DOWN)
         series = MathTex(r"a_1", r"+", r"a_2", r"+", r"a_3", r"+", r"a_4", r"+", r"a_5", r"+", r"a_6", r"+",
-                         r"\cdots").to_edge(UP, MED_SMALL_BUFF)
+                         r"\cdots", color=BLACK).to_edge(UP, MED_SMALL_BUFF)
         maxterm = 6
-        colors = [RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE]
+        colors = [PURE_RED, ORANGE, YELLOW, PURE_GREEN, PURE_BLUE, PURPLE]
         for i in range(0, maxterm):
             series[2 * i].set_color(colors[i])
-        skip = False
+        skip = True
 
         ###########
         # Section 1 - Setting the stage
-        self.next_section(skip_animations=skip)
+        self.next_section(skip_animations=not skip)
         self.play(FadeIn(question, shift=UP), run_time=3)
         self.wait(3)
         self.play(FadeOut(question), FadeIn(series, shift=UP), run_time=3)
@@ -28,9 +29,10 @@ class ITViz(Scene):
 
         ###########
         # Section 2 - Seeing the terms as heights
-        self.next_section(skip_animations=skip)
+        self.next_section(skip_animations=not skip)
         axes = Axes(x_range=[-0.5, 6.5, 1], y_range=[-0.5, f(1) + 0.5, 1], tips=False,
                     x_length=10, y_length=5, x_axis_config={"include_numbers": True}).to_edge(DOWN, MED_LARGE_BUFF)
+        axes.set_color(BLACK)
         self.play(Create(axes))
         self.wait(2)
         tempos = [2, 1.5, 1, 0.5, 0.5, 0.5]
@@ -50,7 +52,7 @@ class ITViz(Scene):
 
         ###########
         # Section 3 - Seeing the series as left or right rectangles
-        self.next_section(skip_animations=skip)
+        self.next_section(skip_animations=not skip)
         lrects = VGroup()
         llabels = VGroup()
         xunit = np.linalg.norm(axes.c2p(1, 0) - axes.c2p(0, 0))
@@ -66,7 +68,7 @@ class ITViz(Scene):
         self.play(LaggedStart(*[Create(rect) for rect in lrects], lag_ratio=3),
                   LaggedStart(*[FadeIn(label) for label in llabels], lag_ratio=3))
 
-        dots = MathTex(r"\cdots", color=WHITE, font_size=44).move_to(axes.c2p(6.3, f(7) / 2))
+        dots = MathTex(r"\cdots", color=BLACK, font_size=44).move_to(axes.c2p(6.3, f(7) / 2))
         self.play(FadeIn(dots))
         self.wait(4)
 
@@ -78,16 +80,17 @@ class ITViz(Scene):
 
         ###########
         # Section 4 - Exploring the first half of statement of the Integral Test (IT)
-        self.next_section(skip_animations=skip)
+        self.next_section(skip_animations=not skip)
 
         graph = axes.plot(f, x_range=[0.9, 6.5, 0.05])
-        flabel = MathTex(r"f(x)", font_size=28).next_to(axes.c2p(0.9, f(0.9)), LEFT, MED_SMALL_BUFF)
+        graph.set_color(BLACK)
+        flabel = MathTex(r"f(x)", color=BLACK, font_size=28).next_to(axes.c2p(0.9, f(0.9)), LEFT, MED_SMALL_BUFF)
         self.play(FadeIn(graph, flabel), run_time=2)
         self.wait(4)
 
-        area = axes.get_area(graph, x_range=[1, 6.5], color=WHITE)
+        area = axes.get_area(graph, x_range=[1, 6.5], color=BLACK)
         comparison = MathTex(r"\int_1^{\infty} f(x) \, dx", r"<", r"\text{series}",
-                             r"\text{If divergent}", r"\Rightarrow", r"\text{divergent}",
+                             r"\text{If divergent}", r"\Rightarrow", r"\text{divergent}", color=BLACK,
                              font_size=36).arrange_in_grid(2, 3).next_to(axes.c2p(3, 3), RIGHT)
         for i in range(0, 6): comparison[2][i].set_color(colors[i])
 
@@ -101,15 +104,15 @@ class ITViz(Scene):
 
         ###########
         # Section 5 - Exploring the second half of statement of the Integral Test (IT)
-        self.next_section(skip_animations=skip)
+        self.next_section(skip_animations=not skip)
         comparison2 = MathTex(r"\int_1^{\infty} f(x) \, dx", r"<", r"\text{series}",
                               r"\text{If convergent}", r"\not\Rightarrow", r"\text{convergent}",
-                              font_size=36).arrange_in_grid(2, 3).next_to(axes.c2p(3, 3), RIGHT)
+                              color=BLACK, font_size=36).arrange_in_grid(2, 3).next_to(axes.c2p(3, 3), RIGHT)
 
         anims = [FadeIn(comparison2[3]), FadeIn(comparison2[4::])]
 
         self.play(LaggedStart(*anims, lag_ratio=5))
-        self.wait(4)
+        self.wait(7)
         self.play(FadeOut(comparison[0:3], comparison2[3::]))
         self.wait(4)
         self.play(allObjects.animate.shift(xunit * LEFT), FadeIn(lastRectAndLabel), run_time=3)
@@ -117,14 +120,14 @@ class ITViz(Scene):
 
         ###########
         # Section 6 - Continue exploring the second half of statement of the Integral Test (IT)
-        self.next_section(skip_animations=skip)
+        self.next_section(skip_animations=not skip)
         comparison3 = MathTex(r"\text{series}-a_1", r"<", r"\int_1^{\infty} f(x) \, dx",
                               r"\text{convergent}", r"\Leftarrow", r"\text{If convergent}",
-                              font_size=36).arrange_in_grid(2, 3).next_to(axes.c2p(3, 3), RIGHT)
+                              color=BLACK, font_size=36).arrange_in_grid(2, 3).next_to(axes.c2p(3, 3), RIGHT)
         for i in range(0, 6): comparison3[0][i].set_color(colors[i])
         comparison3[0][7::].set_color(colors[0])
 
         anims = [FadeIn(comparison3[2]), FadeIn(comparison3[0:2]), FadeIn(comparison3[5]),
                  FadeIn(comparison3[3:5])]
         self.play(LaggedStart(*anims, lag_ratio=5))
-        self.wait(4)
+        self.wait(7)
